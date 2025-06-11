b4a1ef3d971aa5d2f2cfdee7270c860124aef266
Alfred <avocadoaling@gmail.com>
2025-06-11 10:14:09 +0200

    cleanup: formatting and starmap for both process

--- utils/preprocess_video_to_npy.py
+++ utils/preprocess_video_to_npy.py
@@ -4,8 +4,6 @@ import os
 import tyro
 import multiprocessing as mp
 from dataclasses import dataclass
-from concurrent.futures import ProcessPoolExecutor
-from tqdm import tqdm
 
 
 @dataclass
@@ -20,12 +18,11 @@ class Args:
     )
 
 
-# Preprocess one video and save it to a .npy file
 def preprocess_video(
     idx, in_filename, output_path, target_width, target_height, target_fps
 ):
     print(f"Processing video {idx}")
-    # Stream the video as raw RGB24, resize, and set fps
+
     out, _ = (
         ffmpeg.input(in_filename)
         .filter("fps", fps=target_fps, round="up")
@@ -41,17 +38,15 @@ def preprocess_video(
         n_frames, target_height, target_width, 3
     )
 
-    # Create output directory if it doesn't exist
     output_file = f'{output_path}/{in_filename.split("/")[-1].split(".")[0]}.npy'
     if not os.path.exists(os.path.dirname(output_file)):
         os.makedirs(os.path.dirname(output_file))
 
-    # Save as .npy
     np.save(output_file, frames)
     print(f"Saved {n_frames} frames to {output_file} with shape {frames.shape}")
 
 
-def get_n_from_file(filename, directory):
+def get_meta_data(filename, directory):
     filepath = os.path.join(directory, filename)
     arr = np.load(filepath, mmap_mode="r")
     return filepath, arr.shape[0]
@@ -59,9 +54,12 @@ def get_n_from_file(filename, directory):
 
 def main():
     args = tyro.cli(Args)
+
     output_path = f"{args.output_path}/{args.target_fps}fps_{args.target_width}x{args.target_height}"
     print(output_path)
 
+    print(f"Number of processes: {mp.cpu_count()}")
+    print("Converting mp4 to npy files...")
     pool_args = [
         (
             idx,
@@ -75,30 +73,24 @@ def main():
         if in_filename.endswith(".mp4") or in_filename.endswith(".webm")
     ]
 
-    print(f"Number of processes: {mp.cpu_count()}")
-    print("Starting to process videos...")
     with mp.Pool(processes=mp.cpu_count()) as pool:
         pool.starmap(preprocess_video, pool_args)
+    print("Done converting mp4 to npy files")
 
-    # Create metadata file
     print("Creating metadata file...")
     metadata = []
-    npy_files = [
-        f for f in os.listdir(output_path) if f.endswith(".npy") and f != "metadata.npy"
+    filenames = [
+        filename
+        for filename in os.listdir(output_path)
+        if filename.endswith(".npy") and filename != "metadata.npy"
     ]
-    with ProcessPoolExecutor() as executor:
-        results = list(
-            tqdm(
-                executor.map(
-                    get_n_from_file, npy_files, [output_path] * len(npy_files)
-                ),
-                total=len(npy_files),
-            )
-        )
+    pool_args = [(filename, output_path) for filename in filenames]
+
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        results = list(pool.starmap(get_meta_data, pool_args))
         metadata = [{"path": path, "length": length} for path, length in results]
     np.save(output_path + "/metadata.npy", metadata)
-
-    print("Done")
+    print(f"Saved {len(metadata)} videos to {output_path}")
 
 
 if __name__ == "__main__":
