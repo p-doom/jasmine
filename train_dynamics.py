9938b6badb594d49ad5de4f2b9d631ad6409ceba
Franz Srambical <79149449+emergenz@users.noreply.github.com>
2025-09-24 12:48:37 +0200

    Update train_dynamics.py
    Co-authored-by: mihir <78321484+maharajamihir@users.noreply.github.com>

--- train_dynamics.py
+++ train_dynamics.py
@@ -522,8 +522,8 @@ def main(args: Args) -> None:
                 "mask": jnp.ones_like(tokens_full_frame[:, -1]),
             }
             if lam_indices_E is not None:
-                lam_indices_F = lam_indices_E.reshape((-1, args.seq_len - 1))[:, -1]
-                step_outputs["lam_indices"] = lam_indices_F
+                lam_indices_B = lam_indices_E.reshape((-1, args.seq_len - 1))[:, -1]
+                step_outputs["lam_indices"] = lam_indices_B
 
             loss_full_frame, metrics_full_frame = _calculate_step_metrics(
                 step_outputs, gt[:, -1], args.num_actions, args.num_patch_latents
